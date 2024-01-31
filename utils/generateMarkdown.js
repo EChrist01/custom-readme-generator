92ed15bda3b0e941d526b8061ef6bb903cb1c61c
EChrist01 <ethanchrist02@gmail.com>
2024-01-31 10:05:35 -0700

    file update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,11 @@
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "";
+  } else if (license === "Apache") {
+    return "";
+  } 
+}
 
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
